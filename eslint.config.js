ac366aac04aaf405ed76a59e2f0a158cd2a58741
BeomSu Kim <33516980+Indigochi1d@users.noreply.github.com>
2024-10-22 21:04:41 +0900

    chore: add rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import react from "eslint-plugin-react";
 
 export default tseslint.config(
     {ignores: ['dist']},
@@ -11,7 +11,8 @@ export default tseslint.config(
         extends: [
             js.configs.recommended,
             ...tseslint.configs.recommendedTypeChecked,
-            ...tseslint.configs.stylisticTypeChecked,],
+            ...tseslint.configs.stylisticTypeChecked,
+        ],
         files: ['**/*.{ts,tsx}'],
         languageOptions: {
             ecmaVersion: 2020,
@@ -26,14 +27,18 @@ export default tseslint.config(
             'react-refresh': reactRefresh,
             react,
         },
-        settings: {react: {version: '18.3'}},
+        settings: {
+            react: {
+                version: '18.3',
+            },
+        },
         rules: {
-            ...react.configs.recommended,
-            ...react.configs['jsx-runtime'],
+            ...react.configs.recommended.rules,
+            ...react.configs['jsx-runtime'].rules,
             ...reactHooks.configs.recommended.rules,
             'react-refresh/only-export-components': [
                 'warn',
-                {allowConstantExport: true},
+                { allowConstantExport: true },
             ],
         },
     },
