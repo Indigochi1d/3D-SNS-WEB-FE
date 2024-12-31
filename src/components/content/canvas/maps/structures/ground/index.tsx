a7f90873a360ef71c219b7e6e8bff29c12312a82
BeomSu Kim <33516980+Indigochi1d@users.noreply.github.com>
2025-01-01 06:58:55 +0900

    feat : PineTrees 로드

--- src/components/content/canvas/maps/structures/ground/index.tsx
+++ src/components/content/canvas/maps/structures/ground/index.tsx
@@ -1,11 +1,17 @@
 import Floor from "./elements/Floor.tsx";
 import JungleGym from "./elements/JungleGym.tsx";
+import PineTrees from "./elements/PineTrees.tsx";
 
 export const GroundElements = () => {
     return (
         <>
             <Floor />
             <JungleGym/>
+
+            <PineTrees position={[-30,0,-30]} />
+            <PineTrees position={[-20,0,-20]} />
+            <PineTrees position={[-30,0,-20]} />
+            <PineTrees position={[-20,0,-20]} />
         </>
     )
 }
