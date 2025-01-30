00ce9122cccd7c1a846265be53c65b28648fc367
BeomSu Kim <33516980+Indigochi1d@users.noreply.github.com>
2025-01-31 01:25:33 +0900

    feat: 펭귄 닉네임 추가

--- src/components/content/canvas/maps/structures/ground/elements/npc/Penguin.tsx
+++ src/components/content/canvas/maps/structures/ground/elements/npc/Penguin.tsx
@@ -1,14 +1,24 @@
 import { useGLTF } from "@react-three/drei";
-import { useEffect,useMemo } from "react";
-import { Vector3,Object3D } from "three";
+import { MutableRefObject, useEffect,useMemo, useRef } from "react";
+import { Vector3,Object3D,Group } from "three";
+import { NickNameBoard } from "../../3dUIs/NickNameBoard";
 
 const name = "ground-npc-penguin";
 
 const Penguin :React.FC = () => {
+    const ref:MutableRefObject<Group|null> = useRef(null);
+    const nameRef:MutableRefObject<Group|null> = useRef(null);
+    
     const {scene} = useGLTF("/models/Penguin.glb");
     const position : Vector3 = useMemo(() => new Vector3(0,1.5,-5), []);
 
     useEffect(() => {
+        if(!ref.current || !nameRef.current) return;
+        nameRef.current.position.set(
+            ref.current.position.x,
+            ref.current.position.y+2,
+            ref.current.position.z
+        );
         scene.traverse((mesh:Object3D) => {
             mesh.castShadow = true;
             mesh.receiveShadow = true;
@@ -16,7 +26,9 @@ const Penguin :React.FC = () => {
     }, [position,scene]);
     return (
         <>
+            <NickNameBoard ref={nameRef}text="펭돌이" isNpc={true}/>
             <primitive
+                ref={ref}
                 visible
                 name={name}
                 scale={3}
