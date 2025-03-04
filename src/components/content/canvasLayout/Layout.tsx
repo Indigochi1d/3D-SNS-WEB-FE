57d158eed74ec9aab3ab8ae870c093a21169c9fb
BeomSu Kim <33516980+Indigochi1d@users.noreply.github.com>
2025-03-04 11:25:01 +0900

    feat: SideBar 구현

--- src/components/content/canvasLayout/Layout.tsx
+++ src/components/content/canvasLayout/Layout.tsx
@@ -1,14 +1,27 @@
 import {ReactNode} from "react";
+import { useRecoilValue } from "recoil";
+import { IsLoadCompleteAtom } from "../../../store/PlayersAtom";
+import styled from "styled-components";
+import SideBar from "./canvasUserInterfaces/common/SideBar";
 
 interface CanvasLayoutProps {
     children: ReactNode;
 }
 
+const Wrapper = styled.div`
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    background-color: transparent;
+`;
+
 const CanvasLayout = ({children} :CanvasLayoutProps) => {
+    const isLoadCompleted = useRecoilValue(IsLoadCompleteAtom);
     return (
-        <>
+        <Wrapper>
             {children}
-        </>
+            {isLoadCompleted && <SideBar/>}
+        </Wrapper>
     );
 };
 
