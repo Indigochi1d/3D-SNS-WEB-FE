1313ebd7e82e60de90d2a3406ccb1a3aa1b055f9
BeomSu Kim <33516980+Indigochi1d@users.noreply.github.com>
2025-04-22 00:14:40 +0900

    hotfix: git solve

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,29 +1,32 @@
-import {useRecoilValue} from "recoil";
-import {CharacterSelectFinishedAtom, MeAtom} from "../../store/PlayersAtom.ts";
+import { useRecoilValue } from "recoil";
+import {
+  CharacterSelectFinishedAtom,
+  MeAtom,
+} from "../../store/PlayersAtom.ts";
 import MainCanvas from "./canvas/MainCanvas.tsx";
 import CanvasLayout from "./canvasLayout/Layout.tsx";
 import Lobby from "./lobby/Lobby.tsx";
-import {SocketStatusAtom} from "../../store/SocketAtom.ts";
-import ServerOffline from "../utilComponents/ErrorComponent/ServerOffline.tsx";
+import { SocketStatusAtom } from "../../store/SocketAtom.ts";
+import NotFound from "../utilComponents/ErrorComponent/NotFound.tsx";
 
 export const Content = () => {
-    const characterSelectFinished: boolean = useRecoilValue(CharacterSelectFinishedAtom);
-    const me = useRecoilValue(MeAtom);
-    const socketStatus = useRecoilValue(SocketStatusAtom);
+  const characterSelectFinished: boolean = useRecoilValue(
+    CharacterSelectFinishedAtom
+  );
+  const me = useRecoilValue(MeAtom);
+  const socketStatus = useRecoilValue(SocketStatusAtom);
+  const serverStatus = socketStatus.isConnected;
 
-    if (characterSelectFinished && me && socketStatus.isConnected) {
-        return (
-            <CanvasLayout>
-                <MainCanvas />
-            </CanvasLayout>
-        )
-    }
-    if(!socketStatus.isConnected){
-        return(
-            <ServerOffline/>
-        )
-    }
-    
-    return <Lobby/>;
+  if (characterSelectFinished && me && socketStatus.isConnected) {
+    return (
+      <CanvasLayout>
+        <MainCanvas />
+      </CanvasLayout>
+    );
+  }
+  if (!serverStatus) {
+    return <NotFound serverStatus={serverStatus} />;
+  }
 
-}
+  return <Lobby />;
+};
